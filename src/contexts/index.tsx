0c5fcd05aefd94af65ce43f8310fdcc90a189e88
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 18:53:10 -0300

    refactor: nest AuthProvider within ThemeProvider in AppProviders component

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -3,13 +3,16 @@ import type { PropsWithChildren } from "react"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 
+import AuthProvider from "./AuthContext"
 import ThemeProvider from "./ThemeContext"
 
 export default function AppProviders({ children }: PropsWithChildren) {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   )
