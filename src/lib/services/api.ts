4afc96100947412adbcf3dc421c451e4cd37d48a
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 20:46:10 -0300

    fix(api): update environment variable names for API URL and prefix

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -2,8 +2,8 @@ import type { AxiosInstance } from "axios"
 
 import axios from "axios"
 
-const url = process.env.EXPO_PUBLIC_PROTHEUS_API_URL ?? ""
-const prefix = process.env.EXPO_PUBLIC_PROTHEUS_API_URL_PREFIX ?? ""
+const url = process.env.EXPO_PUBLIC_API_URL ?? ""
+const prefix = process.env.EXPO_PUBLIC_API_URL_PREFIX ?? ""
 
 export const api: AxiosInstance = axios.create({
   baseURL: url + prefix,
