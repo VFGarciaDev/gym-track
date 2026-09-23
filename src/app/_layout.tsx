bdb94bf19e15a89f11652ec88a13184d0921d1dd
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 16:28:30 -0300

    refactor: simplify RootLayout by using AppProviders for context management

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,15 @@
 import "@/styles/global.css"
 import { Stack } from "expo-router"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { SafeAreaProvider } from "react-native-safe-area-context"
 
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider"
+import AppProviders from "@/contexts"
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider mode="dark">
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <SafeAreaProvider>
-          <Stack>
-            <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-            <Stack.Screen name="modal" options={{ title: "Modal", presentation: "modal" }} />
-          </Stack>
-        </SafeAreaProvider>
-      </GestureHandlerRootView>
-    </GluestackUIProvider>
+    <AppProviders>
+      <Stack>
+        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ title: "Modal", presentation: "modal" }} />
+      </Stack>
+    </AppProviders>
   )
 }
