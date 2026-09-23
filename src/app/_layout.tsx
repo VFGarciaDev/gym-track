9c5b6b88d431c357b22a26622d968a9ad92d442d
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 15:40:15 -0300

    refactor: reorganize imports and restore strict mode in tsconfig

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,9 @@
 import "@/styles/global.css"
+import { Stack } from "expo-router"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { Stack } from "expo-router"
-
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider"
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(drawer)"
-}
 
 export default function RootLayout() {
   return (
