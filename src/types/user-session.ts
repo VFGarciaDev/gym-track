8c040a33e42328bfab022c82a57eb46e453d0f55
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 18:51:15 -0300

    refactor: remove accessToken from UserSession type and clean up userSchema

--- src/types/user-session.ts
+++ src/types/user-session.ts
@@ -3,12 +3,11 @@ import z from "zod"
 export const userSchema = z.object({
   name: z.string(),
   email: z.string(),
-  taxId: z.string(),
+  taxId: z.string()
 })
 
 export type User = z.infer<typeof userSchema>
 
 export type UserSession = {
-  accessToken: string
   user: User
 }
