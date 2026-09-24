20a0eaafc20dc81fb3acc541e3130d2b0eb09653
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-24 14:22:31 -0300

    refactor(Container): enhance layout with KeyboardAvoidingView and TouchableWithoutFeedback

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,25 +1,40 @@
 import type { PropsWithChildren } from "react"
+import type { ScrollViewProps } from "react-native"
 
-import { ScrollView } from "react-native"
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from "react-native"
 
-type ContainerProps = PropsWithChildren<{
-  padding?: "default" | "none"
-}>
+import { cn } from "@/lib/utils/cn"
 
-export function Container({ padding = "default", children }: ContainerProps) {
+type ContainerProps = PropsWithChildren<
+  ScrollViewProps & {
+    padding?: "default" | "none"
+  }
+>
+
+export function Container({
+  padding = "default",
+  children,
+  contentContainerClassName,
+  ...props
+}: ContainerProps) {
   const paddingSize = {
     none: "",
     default: "px-6"
   }
 
   return (
-    <ScrollView
-      bounces={false}
-      overScrollMode="never"
-      showsVerticalScrollIndicator={false}
-      contentContainerClassName={paddingSize[padding]}
-    >
-      {children}
-    </ScrollView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <ScrollView
+          bounces={false}
+          overScrollMode="never"
+          showsVerticalScrollIndicator={false}
+          contentContainerClassName={cn(paddingSize[padding], contentContainerClassName)}
+          {...props}
+        >
+          {children}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   )
 }
