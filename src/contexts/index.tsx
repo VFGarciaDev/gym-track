73113778a0f5a9c81410b3cf12b0928ac3b7c11c
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-23 21:02:10 -0300

    refactor(context): remove GestureHandlerRootView wrapper from AppProviders

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,6 +1,5 @@
 import type { PropsWithChildren } from "react"
 
-import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 
 import AuthProvider from "./AuthContext"
@@ -8,12 +7,10 @@ import ThemeProvider from "./ThemeContext"
 
 export default function AppProviders({ children }: PropsWithChildren) {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <ThemeProvider>
-          <AuthProvider>{children}</AuthProvider>
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
