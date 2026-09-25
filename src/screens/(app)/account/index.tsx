e342ae86049ac179448192ba5d683bf3014bc16f
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-09-24 23:20:58 -0300

    feat(account): add AccountContent component for account page feat(progress): add ProgressContent component for progress page

---
+++ src/screens/(app)/account/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container"
+import { Text } from "@/components/ui"
+
+export function AccountContent() {
+  return (
+    <Container>
+      <Text>Account Page</Text>
+    </Container>
+  )
+}
---
+++ src/screens/(app)/progress/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container"
+import { Text } from "@/components/ui"
+
+export function ProgressContent() {
+  return (
+    <Container>
+      <Text>Progress Page</Text>
+    </Container>
+  )
+}
